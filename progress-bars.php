4d90df528c4dadb9d55eae44e29ee4cd43f22a1e
Renan <renan_goncalves@outlook.com.br>
2019-03-05 23:17:47 -0300

    Merge origin/master
    Conflicts:
    	php/entidades/Banner.php

--- progress-bars.php
+++ progress-bars.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
     <link href="assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/libs/css/style.css">
-    <link rel="stylesheet" href="assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <title>Concept - Bootstrap 4 Admin Dashboard Template</title>
 </head>
 
