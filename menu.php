a4b8f7758115da488a53a2ae938ee3c7cfeedfd4
Renan <renan_goncalves@outlook.com.br>
2019-05-15 02:18:18 -0300

    Merge origin/master
    Conflicts:
    	cbanner_time.txt
    	php/controler/compra_parceiros_time.txt

--- menu.php
+++ menu.php
@@ -378,7 +378,7 @@ $possiveis[0] = $rtc;
                     <?php } ?>
                         <?php if ($usuario->temPermissao(Sistema::P_IA_CHAT()->m("C"))) { ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="ia_chat.php" ><button class="btn btn-warning" onmousedown="tutorial('Ia Chat', 'Aqui voce consegue alterar a inteligencia artificial do chat')" style="padding:0px;padding-left:4px;width:20px;height:20px;display:inline;margin:0px">&nbsp<i class="fas fa-info"></i></button>&nbsp<i class="fas fa-telegram"></i>Arvore Chat</a>
+                            <a class="nav-link" href="ia_chat.php" ><button class="btn btn-warning" onmousedown="tutorial('Ia Chat', 'Aqui voce consegue alterar a inteligencia artificial do chat')" style="padding:0px;padding-left:4px;width:20px;height:20px;display:inline;margin:0px">&nbsp<i class="fas fa-info"></i></button>&nbsp<i class="fas fa-sitemap"></i>Arvore Chat</a>
                         </li>
                     <?php } ?>
                     <?php if ($usuario->temPermissao(Sistema::P_TIPOS_ATIVIDADE()->m("C"))) { ?>
